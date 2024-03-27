3c095b46d63a5ca2cd4d0b5d0389277dd9b694ac
jx <jessie.xie2023@gmail.com>
2024-03-27 16:28:06 +0000

    convert weather page to hotspot page

--- frontend/src/pages/HotSpotView.tsx
+++ frontend/src/pages/HotSpotView.tsx
@@ -1,86 +1,33 @@
 import { useEffect, useState } from "react"
 import { Col, Container, Row } from "react-bootstrap"
-import { useParams } from "react-router-dom"
-
-interface Weather {
-  latitude: number
-  longitude: number
-  generationtime_ms: number
-  utc_offset_seconds: number
-  timezone: string
-  timezone_abbreviation: string
-  elevation: number
-  current_units: {
-    time: string
-    interval: string
-    temperature_2m: string
-    relative_humidity_2m: string
-    weather_code: string
-    precipitation: string
-  }
-  current: {
-    time: string
-    interval: number
-    temperature_2m: number
-    relative_humidity_2m: number
-    apparent_temperature: number
-    weather_code: number
-    precipitation: number
-  }
-  daily_units: {
-    time: string
-    weather_code: string
-    temperature_2m_max: string
-    temperature_2m_min: string
-    precipitation_sum: string
-    precipitation_probability_max: string
-  }
-  daily: {
-    time: [string, string, string, string, string, string, string]
-    weather_code: [number, number, number, number, number, number, number]
-    temperature_2m_max: [number, number, number, number, number, number, number]
-    temperature_2m_min: [number, number, number, number, number, number, number]
-    precipitation_sum: [number, number, number, number, number, number, number]
-    precipitation_probability_max: [number, number, number, number, number, number, number]
-  }
-}
-
-const hotSpots = [
-  {
-    hotSpotName: "Puerto Piramides",
-    coordinates: {
-      lat: "-42.566667",
-      lon: "-64.283333",
-    },
-  },
-  {
-    hotSpotName: "Victoria",
-    coordinates: {
-      lat: "48.428333",
-      lon: "-123.364722",
-    },
-  },
-  {
-    hotSpotName: "Dulce Gulf",
-    coordinates: {
-      lat: "8.583333",
-      lon: "-83.266667",
-    },
-  },
-]
+import { Link, useParams } from "react-router-dom"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
+import { Icon } from "leaflet"
+import icon from "/favicon.ico"
+import Weather from "../models/view/Weather"
+import HotSpot from "../models/view/Hotspot"
 
 function HotSpotView() {
   const { id } = useParams()
+  const [hotSpot, setHotSpot] = useState<HotSpot>()
   const [weather, setWeather] = useState<Weather>()
   const [loading, setLoading] = useState<boolean>(false)
   const [error, setError] = useState<boolean>(false)
+
+  useEffect(() => {
+    fetch(`http://localhost:5280/hotspots/${id}`)
+      .then((response) => response.json())
+      .then((data) => setHotSpot(data[0]))
+      .catch(() => setError(true))
+      .finally(() => setLoading(false))
+  }, [id])
 
   const handleSubmit = () => {
     setWeather(undefined)
     setError(false)
 
-    const lat = hotSpots[parseInt(id!)].coordinates.lat
-    const lon = hotSpots[parseInt(id!)].coordinates.lon
+    const lat = hotSpot?.latitude
+    const lon = hotSpot?.longitude
 
     if (lat && lon) {
       fetch(
@@ -92,8 +39,7 @@ function HotSpotView() {
         .finally(() => setLoading(false))
     }
   }
-
-  useEffect(handleSubmit, [id])
+  useEffect(handleSubmit, [hotSpot])
 
   const arr = []
   if (weather) {
@@ -111,34 +57,72 @@ function HotSpotView() {
     return x.date, x.minTemp, x.maxTemp, x.rain, x.icon
   })
 
+  const customIcon = new Icon({
+    iconUrl: icon,
+    iconSize: [24, 24],
+  })
+
   return (
-    <main className="d-flex align-items-center justify-content-center text-center">
-      {weather && (
-        <div>
-          <br />
-          <h5>Current weather:</h5>
-          <img src={`${import.meta.env.BASE_URL}weather-icons/icon${weather.current.weather_code}.png`} width="35" />
-          <p>Temp: {weather.current.temperature_2m} °C</p>
-          <br></br>
-          <h5>Weather forecast for the next 7 days:</h5>
-          <Container className="d-flex flex-row justify-content-between">
-            <Row md={7}>
-              {arr.map((x, i) => (
-                <Col key={i}>
-                  <p>{new Date(x.date).toDateString()}</p>
-                  <img src={`${import.meta.env.BASE_URL}weather-icons/icon${x.icon}.png`} width="35" />
-                  <p>MinTemp: {Math.round(x.minTemp)}°C</p>
-                  <p>MaxTemp: {Math.round(x.maxTemp)}°C</p>
-                  <p>Rain probability: {x.rain}%</p>
-                </Col>
-              ))}
-            </Row>
-          </Container>
-        </div>
+    <>
+      {hotSpot && (
+        <>
+          <h1>{hotSpot.name}</h1>
+          <h2>Country: {hotSpot.country}</h2>
+
+          <MapContainer style={{ height: "20rem" }} center={[26.115, -16.523]} zoom={1} scrollWheelZoom={true}>
+            <TileLayer
+              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+            />
+            <Marker key={hotSpot.id} position={[Number(hotSpot.latitude), Number(hotSpot.longitude)]} icon={customIcon}>
+              <Popup>{hotSpot.name}</Popup>
+            </Marker>
+          </MapContainer>
+
+          <main className="d-flex align-items-center justify-content-center text-center">
+            {weather && (
+              <div>
+                <br />
+                <h5>Current weather:</h5>
+                <img
+                  src={`${import.meta.env.BASE_URL}weather-icons/icon${weather.current.weather_code}.png`}
+                  width="35"
+                />
+                <p>Temp: {weather.current.temperature_2m} °C</p>
+                <br></br>
+                <h5>Weather forecast for the next 7 days:</h5>
+                <Container className="d-flex flex-row justify-content-between">
+                  <Row md={7}>
+                    {arr.map((x, i) => (
+                      <Col key={i}>
+                        <p>{new Date(x.date).toDateString()}</p>
+                        <img src={`${import.meta.env.BASE_URL}weather-icons/icon${x.icon}.png`} width="35" />
+                        <p>MinTemp: {Math.round(x.minTemp)}°C</p>
+                        <p>MaxTemp: {Math.round(x.maxTemp)}°C</p>
+                        <p>Rain probability: {x.rain}%</p>
+                      </Col>
+                    ))}
+                  </Row>
+                </Container>
+              </div>
+            )}
+            {loading && <p>Loading...</p>}
+            {error && <p>Couldn't load weather data</p>}
+          </main>
+
+          <ul>
+            {hotSpot.viewingSuggestions.map((suggestion) => (
+              <li>
+                {" "}
+                <Link to={suggestion.species.wikiLink}>Species: {suggestion.species.name}</Link>, Time of year:{" "}
+                {suggestion.timeOfYear}, Platforms: {suggestion.platforms},
+                <img src={suggestion.species.exampleImageUrl} width="300" />
+              </li>
+            ))}
+          </ul>
+        </>
       )}
-      {loading && <p>Loading...</p>}
-      {error && <p>Couldn't load weather data</p>}
-    </main>
+    </>
   )
 }
 
