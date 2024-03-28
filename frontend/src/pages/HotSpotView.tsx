7e5375643212503df8fd6647cdc6cb2e783353e6
Pavli <paola.dalbore@softwire.com>
2024-03-28 17:58:46 +0000

    finish hotspot view

--- frontend/src/pages/HotSpotView.tsx
+++ frontend/src/pages/HotSpotView.tsx
@@ -1,11 +1,13 @@
 import { useEffect, useState } from "react"
-import { Col, Container, Row } from "react-bootstrap"
-import { Link, useParams } from "react-router-dom"
+import { Card, Col, Container, Row } from "react-bootstrap"
+import { useParams } from "react-router-dom"
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 import { Icon } from "leaflet"
 import icon from "/favicon.ico"
 import Weather from "../models/view/Weather"
 import HotSpot from "../models/view/HotSpot"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { faDroplet, faTemperatureArrowDown, faTemperatureArrowUp } from "@fortawesome/free-solid-svg-icons"
 
 function HotSpotView() {
   const { id } = useParams()
@@ -65,62 +67,97 @@ function HotSpotView() {
   return (
     <>
       {hotSpot && (
-        <>
-          <h1>{hotSpot.name}</h1>
-          <h2>Country: {hotSpot.country}</h2>
-
-          <MapContainer style={{ height: "20rem" }} center={[26.115, -16.523]} zoom={1} scrollWheelZoom={true}>
-            <TileLayer
-              attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            <Marker key={hotSpot.id} position={[Number(hotSpot.latitude), Number(hotSpot.longitude)]} icon={customIcon}>
-              <Popup>{hotSpot.name}</Popup>
-            </Marker>
-          </MapContainer>
-
-          <main className="d-flex align-items-center justify-content-center text-center">
-            {weather && (
-              <div>
-                <br />
-                <h5>Current weather:</h5>
-                <img
-                  src={`${import.meta.env.BASE_URL}weather-icons/icon${weather.current.weather_code}.png`}
-                  width="35"
+        <Container>
+          <h1 className="text-center">{hotSpot.name}</h1>
+          <h2 className="text-center mb-4">{hotSpot.country}</h2>
+          <Row>
+            <Col xs={12} md={4} className="d-flex flex-column align-items-center">
+              <h5 className="text-center">Hotspot location</h5>
+              <MapContainer
+                style={{ height: "20rem", maxWidth: "100%", width: "32rem" }}
+                center={[30, hotSpot.longitude]}
+                zoom={1}
+                scrollWheelZoom={true}
+              >
+                <TileLayer
+                  attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <p>Temp: {weather.current.temperature_2m} °C</p>
-                <br></br>
-                <h5>Weather forecast for the next 7 days:</h5>
-                <Container className="d-flex flex-row justify-content-between">
-                  <Row md={7}>
-                    {arr.map((x, i) => (
-                      <Col key={i}>
-                        <p>{new Date(x.date).toDateString()}</p>
-                        <img src={`${import.meta.env.BASE_URL}weather-icons/icon${x.icon}.png`} width="35" />
-                        <p>MinTemp: {Math.round(x.minTemp)}°C</p>
-                        <p>MaxTemp: {Math.round(x.maxTemp)}°C</p>
-                        <p>Rain probability: {x.rain}%</p>
-                      </Col>
+                <Marker
+                  key={hotSpot.id}
+                  position={[Number(hotSpot.latitude), Number(hotSpot.longitude)]}
+                  icon={customIcon}
+                >
+                  <Popup>{hotSpot.name}</Popup>
+                </Marker>
+              </MapContainer>
+            </Col>
+            <Col xs={12} md={8} className="d-flex flex-column align-items-center">
+              {weather && (
+                <>
+                  <h5 className="text-center mt-3 mt-md-0">Weather forecast</h5>
+                  <div className="d-flex flex-wrap justify-content-center gap-3">
+                    {arr.map((x) => (
+                      <div key={x.date} className="shadow p-3">
+                        <Row style={{ width: "10rem" }}>
+                          <Col className="text-start">
+                            <h5 className="mb-0">{new Date(x.date).toDateString().split(" ")[0]}</h5>
+                            <p>
+                              {new Date(x.date).toDateString().split(" ")[2]}{" "}
+                              {new Date(x.date).toDateString().split(" ")[1]}{" "}
+                            </p>
+                            <p className="mb-0">
+                              <FontAwesomeIcon icon={faTemperatureArrowUp} className="text-danger" />{" "}
+                              {Math.round(x.maxTemp)}°C
+                            </p>
+                            <p className="mb-0">
+                              <FontAwesomeIcon icon={faTemperatureArrowDown} className="text-primary" />{" "}
+                              {Math.round(x.minTemp)}°C
+                            </p>
+                          </Col>
+                          <Col className="d-flex flex-column justify-content-between align-items-end text-end">
+                            <img src={`${import.meta.env.BASE_URL}weather-icons/icon${x.icon}.png`} width="48" />
+                            <p className="mb-0">
+                              <FontAwesomeIcon icon={faDroplet} className="text-primary" /> {x.rain}%
+                            </p>
+                          </Col>
+                        </Row>
+                        {/* <p><img src={waterDrop} width="35"/> {x.rain}%</p> */}
+                      </div>
                     ))}
-                  </Row>
-                </Container>
-              </div>
-            )}
-            {loading && <p>Loading...</p>}
-            {error && <p>Couldn't load weather data</p>}
-          </main>
-
-          <ul>
+                  </div>
+                </>
+              )}
+              {loading && <p>Loading...</p>}
+              {error && <p>Couldn't load weather data</p>}
+            </Col>
+          </Row>
+          <h5 className="mt-4 text-center">Viewing suggestions for this hotspot</h5>
+          <ul className="list-unstyled d-flex flex-wrap justify-content-center gap-3 pb-4 text-center">
             {hotSpot.viewingSuggestions.map((suggestion) => (
               <li>
-                {" "}
-                <Link to={suggestion.species.wikiLink}>Species: {suggestion.species.name}</Link>, Time of year:{" "}
-                {suggestion.timeOfYear}, Platforms: {suggestion.platforms},
-                <img src={suggestion.species.exampleImageUrl} width="300" />
+                <Card style={{ width: "18rem" }}>
+                  <Card.Img
+                    className="bg-dark"
+                    variant="top"
+                    src={suggestion.species.exampleImageUrl}
+                    style={{ height: "10rem", objectFit: "contain" }}
+                  />
+                  <Card.Body>
+                    <Card.Title>{suggestion.species.name}</Card.Title>
+                    Time of year: {suggestion.timeOfYear}
+                    <br />
+                    Platforms: {suggestion.platforms}
+                    <br />
+                    <a href={suggestion.species.wikiLink} target="_blank">
+                      Find out more about {suggestion.species.name}s
+                    </a>
+                  </Card.Body>
+                </Card>
               </li>
             ))}
           </ul>
-        </>
+        </Container>
       )}
     </>
   )
