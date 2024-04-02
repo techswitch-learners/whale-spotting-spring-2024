46466fa3b67324c150c41e8ac7a521a5a6f9e1b1
Sundaraswara Munieeswaran <kvrnandhini@gmail.com>
2024-04-02 18:19:07 +0530

    added falmingo to error 404 page

--- frontend/src/pages/Error404.tsx
+++ frontend/src/pages/Error404.tsx
@@ -1,19 +1,22 @@
 import { useContext, useEffect } from "react"
 import { BackgroundContext } from "../App"
-import Error404Whale from "../assets/Error404.gif"
+import flamingo from "../assets/flamingo.gif"
 import NavbarBottom from "../components/NavbarBottom"
 
 const Error404 = () => {
   const backgroundContext = useContext(BackgroundContext)
 
   useEffect(() => {
-    backgroundContext.setBackground(`url("${Error404Whale}")`)
+    backgroundContext.setBackground("linear-gradient(to bottom, white, #f69bd5 85%, #ff6ac9)")
   }, [backgroundContext])
 
   return (
     <>
-      <div className="Home row min-h-100">
-        <h1 className="text-center">404: Whale not found</h1>
+      <div className="Home min-h-100 d-flex flex-column justify-content-center align-items-center">
+        <h1 className="text-center mb-5" style={{ color: "#e052ac" }}>
+          404: Whale not found
+        </h1>
+        <img src={flamingo} style={{ width: "22.5rem", height: "22.5rem" }} />
       </div>
       <NavbarBottom />
     </>
