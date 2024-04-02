5c37b74c3d1f8214c5860a0ca95945199cd26ffc
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-04-02 22:37:23 +0100

    ws08:Refactor: Removing unnecessary comments

--- backend/Models/Response/ReactionResponse.cs
+++ backend/Models/Response/ReactionResponse.cs
@@ -4,13 +4,7 @@ namespace WhaleSpotting.Models.Response;
 
 public class ReactionResponse
 {
-    // public required string Type { get; set; }
-
-    // public required int UserId { get; set; }
-
     public required int SightingId { get; set; }
     public required Dictionary<string, int> Reactions { get; set; }
     public string? CurrentUserReaction { get; set; }
-
-    // public required DateTime Timestamp { get; set; }
 }
