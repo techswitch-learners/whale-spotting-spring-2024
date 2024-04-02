7dbf75e2e0b6f2d690b8385c56f62cff94fab1d1
Sundaraswara Munieeswaran <kvrnandhini@gmail.com>
2024-04-02 20:24:16 +0530

    Error 403 and Error 404 pages

--- frontend/src/pages/Error403.tsx
+++ frontend/src/pages/Error403.tsx
@@ -1,19 +1,21 @@
 import { useContext, useEffect } from "react"
 import { BackgroundContext } from "../App"
-import Error404Whale from "../assets/Error404.gif"
+import Error403Whale from "../assets/Error403.png"
 import NavbarBottom from "../components/NavbarBottom"
 
 const Error403 = () => {
   const backgroundContext = useContext(BackgroundContext)
 
   useEffect(() => {
-    backgroundContext.setBackground(`url("${Error404Whale}")`)
+    backgroundContext.setBackground(`url("${Error403Whale}")`)
   }, [backgroundContext])
 
   return (
     <>
-      <div className="Home row min-h-100">
-        <h1 className="text-center">404: Whale not found</h1>
+      <div className="Home min-h-100 ">
+        <h1 className="text-center" style={{ color: "gold", fontSize: "24px" }}>
+          403: You are not authorised to view this page
+        </h1>
       </div>
       <NavbarBottom />
     </>
