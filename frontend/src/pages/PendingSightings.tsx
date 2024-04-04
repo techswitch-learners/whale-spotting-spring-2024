209cbb42a5e431f939e6a85a40011f32b44b9952
Anastasia C <clarkanastasia@outlook.com>
2024-04-04 13:56:05 +0100

    update pending sightings

--- frontend/src/pages/PendingSightings.tsx
+++ frontend/src/pages/PendingSightings.tsx
@@ -4,96 +4,80 @@ import { getPendingSightings, verifySighting } from "../api/backendClient"
 import { AuthContext, BackgroundContext } from "../App"
 import { Button, Card, Form, FormGroup } from "react-bootstrap"
 
+interface SightingCardProps {
+  id: number
+  sighting: Sighting
+  handleSubmit: (id: number, status: string, setComment: (comment?: string) => void, comment?: string) => void
+}
+function PendingSightingCard({ id, sighting, handleSubmit }: SightingCardProps) {
+  const [comment, setComment] = useState<string>()
+  const handleCommentChange = (value: string) => {
+    setComment(value)
+  }
+
+  return (
+    <Card className="text-start" style={{ width: "15rem" }}>
+      <Card.Img variant="top" src={sighting.imageUrl} style={{ height: "15rem", objectFit: "cover" }} />
+      <Card.Body>
+        <Card.Text>Posted by: {sighting.userName}</Card.Text>
+        <Card.Text>Species: {sighting.species.name}</Card.Text>
+        <Card.Text>Body of Water: {sighting.bodyOfWater}</Card.Text>
+        <Card.Text>Description: {sighting.description}</Card.Text>
+        <Card.Text>Posted on: {sighting.sightingTimestamp.split("T")[0]}</Card.Text>
+      </Card.Body>
+      <Card.Footer className="d-flex justify-content-center align-items-center">
+        <Form>
+          <FormGroup controlId={`commentTextArea${id}`} className="mb-2 text-start">
+            <Form.Label>Comment(optional):</Form.Label>
+            <Form.Control
+              as="textarea"
+              placeholder="Leave your comments"
+              value={comment}
+              onChange={(e) => handleCommentChange(e.target.value)}
+            />
+          </FormGroup>
+          <Button onClick={() => handleSubmit(id, "approve", setComment, comment)}>Approve</Button>
+          <Button className="mx-3" variant="danger" onClick={() => handleSubmit(id, "approve", setComment, comment)}>
+            Reject
+          </Button>
+        </Form>
+      </Card.Footer>
+    </Card>
+  )
+}
+
 const PendingSightings = () => {
   const backgroundContext = useContext(BackgroundContext)
   const [pendingSightings, setPendingSightings] = useState<Sighting[]>()
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(false)
   const [unauthorisedAccess, setUnauthorisedAccess] = useState(false)
-  const [comments, setComments] = useState<string[]>([])
   const authContext = useContext(AuthContext)
 
-  interface SightingCardProps {
-    id: number
-    index: number
-    imageUrl: string
-    userName: string
-    species: string
-    bodyOfWater: string
-    description: string
-    sightingTimestamp: string
-  }
-  function PendingSightingCard({
-    id,
-    index,
-    imageUrl,
-    species,
-    bodyOfWater,
-    description,
-    sightingTimestamp,
-    userName,
-  }: SightingCardProps) {
-    const handleCommentChange = (value: string, index: number) => {
-      const newComments = [...comments]
-      newComments[index] = value
-      setComments(newComments)
-    }
+  const handleSubmit = (id: number, status: string, setComment: (comment?: string) => void, comment?: string) => {
+    setError(false)
+    const approvalStatus = status === "approve" ? 1 : 0
 
-    const handleSubmit = (status: string, id: number, index: number) => {
-      setError(false)
-      const approvalStatus = status === "approve" ? 1 : 0
-      const comment = comments[index]
-
-      verifySighting(
-        {
-          comment: comment ? comment : undefined,
-          approvalStatus: approvalStatus,
-        },
-        id,
-        authContext.cookie.token,
-      )
-        .then((response) => {
-          if (response.ok) {
-            setComments([])
-            setPendingSightings(pendingSightings?.filter((sighting) => sighting.id != id))
-          } else if (response.status === 403 || response.status === 401) {
-            authContext.removeCookie("token")
-            setUnauthorisedAccess(true)
-          }
-        })
-        .catch(() => setError(true))
-    }
-
-    return (
-      <Card className="text-start" style={{ width: "15rem" }}>
-        <Card.Img variant="top" src={imageUrl} style={{ height: "15rem", objectFit: "cover" }} />
-        <Card.Body>
-          <Card.Text>Posted by: {userName}</Card.Text>
-          <Card.Text>Species: {species}</Card.Text>
-          <Card.Text>Body of Water: {bodyOfWater}</Card.Text>
-          <Card.Text>Description: {description}</Card.Text>
-          <Card.Text>Posted on: {sightingTimestamp}</Card.Text>
-        </Card.Body>
-        <Card.Footer className="d-flex justify-content-center align-items-center">
-          <Form>
-            <FormGroup controlId={`commentTextArea${id}`} className="mb-2 text-start">
-              <Form.Label>Comment(optional):</Form.Label>
-              <Form.Control
-                as="textarea"
-                placeholder="Leave your comments"
-                value={comments[index]}
-                onChange={(e) => handleCommentChange(e.target.value, index)}
-              />
-            </FormGroup>
-            <Button onClick={() => handleSubmit("approve", id, index)}>Approve</Button>
-            <Button className="mx-3" variant="danger" onClick={() => handleSubmit("reject", id, index)}>
-              Reject
-            </Button>
-          </Form>
-        </Card.Footer>
-      </Card>
+    verifySighting(
+      {
+        comment,
+        approvalStatus,
+      },
+      id,
+      authContext.cookie.token,
     )
+      .then((response) => {
+        if (response.ok) {
+          setComment(undefined)
+          setPendingSightings(pendingSightings?.filter((sighting) => sighting.id != id))
+        } else if (response.status === 403 || response.status === 401) {
+          authContext.removeCookie("token")
+          setUnauthorisedAccess(true)
+        }
+      })
+      .catch(() => setError(true))
   }
+
   function getData() {
     setLoading(true)
     setError(false)
@@ -122,18 +106,8 @@ const PendingSightings = () => {
       <h1>Pending Sightings</h1>
       {pendingSightings && (
         <div className="d-flex flex-wrap justify-content-center gap-4 my-4">
-          {pendingSightings.map((sighting, index) => (
-            <PendingSightingCard
-              key={sighting.id}
-              id={sighting.id}
-              index={index}
-              userName={sighting.userName}
-              imageUrl={sighting.imageUrl}
-              species={sighting.species.name}
-              bodyOfWater={sighting.bodyOfWater}
-              description={sighting.description}
-              sightingTimestamp={sighting.sightingTimestamp.split("T")[0]}
-            />
+          {pendingSightings.map((sighting) => (
+            <PendingSightingCard key={sighting.id} id={sighting.id} sighting={sighting} handleSubmit={handleSubmit} />
           ))}
         </div>
       )}
