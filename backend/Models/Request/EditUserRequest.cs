365e8b97e92d032c447b9afb7ae0dcbee6342414
Anastasia C <clarkanastasia@outlook.com>
2024-04-04 13:07:38 +0100

    delete edit user request

--- backend/Models/Request/EditUserRequest.cs
+++
@@ -1,9 +0,0 @@
-namespace WhaleSpotting.Models.Request;
-
-public class EditUserRequest
-{
-    public string? UserName { get; set; }
-    public string? ProfileImageUrl { get; set; }
-    public string? Email { get; set; }
-    public string? PhoneNumber { get; set; }
-}
