b527d87eb77822fe6904c54367512e28cda3cae7
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-04-05 12:41:30 +0100

    WS-11:Achievements: Adding new badges and fixing spellings

--- frontend/src/pages/Faqs.tsx
+++ frontend/src/pages/Faqs.tsx
@@ -5,6 +5,7 @@ import { Link } from "react-router-dom"
 import { BackgroundContext } from "../App"
 import { getAchievements } from "../api/backendClient"
 import Achievement from "../models/view/Achievement"
+import "./Faqs.scss"
 
 const Faqs = () => {
   const backgroundContext = useContext(BackgroundContext)
@@ -43,13 +44,13 @@ const Faqs = () => {
       <Accordion flush>
         <Accordion.Item eventKey="0">
           <Accordion.Header>Q1. How can I get experience?</Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className="align-left">
             You can gain experience by adding whale sighting reports. Every time you add a sighting report, you earn 50
             experience points (exp). To upload your sighting, please use the following link:
             <Link to={"/sightings/add"}>Upload Your Sighting</Link> Additionally, if your sighting report receives
             reactions from other viewers, you can earn extra experience points. Here's how the reactions translate into
             experience:
-            <ListGroup>
+            <ListGroup className="align-left">
               <ListGroup.Item>🤩 : +10 exp</ListGroup.Item>
               <ListGroup.Item>😊 : +6 exp</ListGroup.Item>
               <ListGroup.Item>😐 : +3 exp</ListGroup.Item>
@@ -61,12 +62,12 @@ const Faqs = () => {
 
         <Accordion.Item eventKey="1">
           <Accordion.Header>Q2. How can I get badges?</Accordion.Header>
-          <Accordion.Body>
+          <Accordion.Body className="align-left">
             Badges are a way to showcase your progress and achievement levels in our whale spotting community. You gain
             experience points (exp) by adding sighting reports and receiving reactions. As you accumulate experience,
             you get promoted to new achievement levels, each with its unique badge. Here's a brief overview of the
             achievement levels and the minimum experience needed for each:
-            <Table striped bordered hover>
+            <Table className="align-left" striped bordered hover>
               <thead>
                 <tr>
                   <th>Badge</th>
