9c24a3842252637ebeba0e6480968650db200853
Nandini Polavarapu <nandini.p8892@gmail.com>
2024-04-05 12:40:06 +0100

    WS11-Achievements: Data fix in the excel for Narwhal

---
+++ backend/Migrations/20240405113818_AchievementsDataFix_2.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WhaleSpotting.Migrations
+{
+    /// <inheritdoc />
+    public partial class AchievementsDataFix_2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "BadgeImageUrl", "Name" },
+                values: new object[] { "narwhal.png", "Narwhal" }
+            );
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Achievements",
+                keyColumn: "Id",
+                keyValue: 4,
+                columns: new[] { "BadgeImageUrl", "Name" },
+                values: new object[] { "narwhale.png", "Narwhale" }
+            );
+        }
+    }
+}
